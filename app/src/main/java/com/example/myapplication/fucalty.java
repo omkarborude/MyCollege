6b74d890aba10909ec91a29ceb13b37700df0d48
omkarborude <borudeomkar41@gmail.com>
2020-02-19 20:50:56 +0530

    new
    new

--- app/src/main/java/com/example/myapplication/fucalty.java
+++ app/src/main/java/com/example/myapplication/fucalty.java
@@ -9,6 +9,6 @@ public class fucalty extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_2);
+        setContentView(R.layout.faculty);
     }
 }
